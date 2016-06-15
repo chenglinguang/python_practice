d4d85b81b1b57626e133de2cca2686a5bf592540
root <root@localhost.localdomain>
2016-06-15 16:51:01 +0800

    ListNode defination

---
+++ ListNode.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3 
+
+#-*- coding:utf-8 -*-
+
+__author__='Cheng Linguang'
+
+class ListNode(object):
+
+    def __init__(self,val):
+        self.data=val
+        self.next=None
+    
+    def get_data(self):
+        return self.data
+    
+    def get_next(self):
+        return self.next
+    
+    def set_data(self,val):
+        self.data=val
+
+    def set_next(self,val)
+        self.next=val
+        
