112d2edac031739a787c1c1d65a3101a7d23f5af
chenglinguang <clg19881220@163.com>
2016-06-18 23:06:44 +0800

    define a Linked list in python

---
+++ Linked_list_python.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3 
+#-*- coding:utf-8 -*-
+
+#write a Linked list in python
+
+#Every Node has a value and a pointer to the next node . When a node is first created ,it's assigned a given value and does not point to any# other node
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+
+
+
+
