d55e90acce84c2b2a85cd6b669ff113eeecb5d6f
chenglinguang <clg19881220@163.com>
2016-06-22 15:38:46 +0800

    super usage

---
+++ super_usage/child_parent_super.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3 
+
+#-*- coding:utf-8 -*-
+
+class FooParent(object):
+    def __init__(self):
+        self.parent='I\'am the parent.'
+        print('Parent')
+
+    def bar(self,message):
+        print (message,'from parent')
+
+class FooChild(FooParent):
+    def __init__(self):
+        super(FooChild,self).__init__()
+        print('child')
+    def bar(self,message):
+        super(FooChild,self).bar(message)
+        print ('Child bar function')
+        print (self.parent)
+
+
+if __name__=='__main__':
+    fooChild=FooChild()
+    fooChild.bar('Hello World')
+
+
+
+
+
+
+
+
