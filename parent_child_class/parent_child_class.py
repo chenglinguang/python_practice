2762cf68f5777f6adc7c3358c74fd232ce2ec7cf
chenglinguang <clg19881220@163.com>
2016-07-16 16:49:22 +0800

    add a small question

--- parent_child_class/parent_child_class.py
+++ parent_child_class/parent_child_class.py
@@ -18,3 +18,17 @@ Parent.x=3
 print (Parent.x,Child1.x,Child2.x)
 
 
+def div1(x,y):
+    print("%s/%s = %s" % (x, y, x/y))
+
+def div2(x,y):
+    print("%s//%s = %s" % (x, y, x//y))
+
+div1(5,2)
+div1(5.,2)
+div2(5,2)
+div2(5.,2.)
+
+
+
+
